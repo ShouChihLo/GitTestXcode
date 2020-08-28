2c1e6e33957b81bef65e76d56b41623e0f17293a
Shou-Chih Lo <sclo@gms.ndhu.edu.tw>
2020-08-28 10:15:32 +0800

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by 羅壽之 on 2020/8/28.
+//  Copyright © 2020 羅壽之. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
